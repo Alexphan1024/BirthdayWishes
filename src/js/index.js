338c7871b1401a66a9b25cd5267ae87afe6b9eef
Alexphan1024 <31673789+Alexphan1024@users.noreply.github.com>
2023-04-05 22:55:38 -0700

    mandy bday

--- src/js/index.js
+++ src/js/index.js
@@ -21,15 +21,15 @@ thirdCanvas.setAttribute("style","display:none");
 let containerToggleOne = setTimeout(function(){
 	firstSlideContainer.setAttribute("style","display:none");
 	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+},4000);
 
 
 let removeFirstSlide = setTimeout(function(){
 	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
 	secondCanvas.setAttribute("style","display:block");
-},6500);
+},6000);
 
 let removeSecondCanvas = setTimeout(function(){
 	secondCanvas.setAttribute("style","display:none");
 	thirdCanvas.setAttribute("style","display:block")
-},9800)
+},8500)
